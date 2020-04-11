b3c2dccfe2a88bb6d405dd6c58e26fc3c8bf97d6
szabolcspall <szabolcs.pall@shpock.com>
2020-04-11 23:11:29 +0200

    build + VS

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float panSpeed;
+    private float panDetect = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveY = Camera.main.transform.position.y;
+
+        float xPos = Input.mousePosition.x;
+        float yPos = Input.mousePosition.y;
+
+        if (Input.GetKey(KeyCode.A) || xPos > 0 && xPos < panDetect)
+        {
+            moveX -= panSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width - panDetect)
+        {
+            moveX += panSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - panDetect)
+        {
+            moveY += panSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < panDetect)
+        {
+            moveY -= panSpeed;
+        }
+
+        Vector3 newPos = new Vector3(moveX, moveY, 0);
+
+        Camera.main.transform.position = newPos;
+    }
+}
